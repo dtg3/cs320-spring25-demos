a6e1b53ebb20ae7f8250fac6f1912b6f28c72f4f
Drew Guarnera <dguarnera@wooster.edu>
2025-03-10 13:57:59 -0400

    Add comments to flask example

--- flask-content/flask-demo/app.py
+++ flask-content/flask-demo/app.py
@@ -1,5 +1,7 @@
+# Necessary import for Flask stuff
 from flask import Flask, render_template, request
 
+# This initializes the app
 app = Flask(__name__)
 
 # List of dictionaries to simulate a database
@@ -29,16 +31,24 @@ grocery_list = [
     }
 ]
 
+# These are routes. Routes allow you to create "paths" for your website to display different content.
+#   The "methods" is a list of strings that represent the HTTP Verbs GET, POST, UPDATE, etc. We'll likely
+#   only need GET and POST. GET is a non-modification operation. You are loading or requesting existing content.
+#   POST is for when you want data to be sent along to your web service.
+
+# Routes can return a template (HTML content) sent to the user.
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
 
 
+# You can also pass data along to the template to render information from another source (like our list of dictionaries)
 @app.route('/select-list/', methods=["GET"])
 def select_list():
     return render_template("select-list-demo.html", groceries=grocery_list)
 
 
+# You can also return regular strings, or even a set of data (often in the JSON format).
 @app.route('/select-result/', methods=["POST"])
 def select_list_result():
     select_list_data = request.form.get("groceries")
@@ -57,5 +67,6 @@ def delete_item():
     return render_template("delete-demo.html", groceries=grocery_list)
 
 
+# This is where the application launches and is really all you need for our purposes.
 if __name__ == '__main__':
     app.run(debug=True, host="localhost")
